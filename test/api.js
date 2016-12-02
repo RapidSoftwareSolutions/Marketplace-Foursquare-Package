4b8c9faf022ee365f2fe26ecf62ad4141922318d
Dima Shirokov <dima.shirokoff@rapidapi.com>
2016-12-02 18:05:52 +0200

    redeploy

--- test/api.js
+++
@@ -1,9 +0,0 @@
-const app     = require('../index');
-const assert  = require('chai').assert;
-const request = require('supertest-as-promised');
-
-describe('Foursquare API', () => {
-    it('...', function() {
-      console.log('Todo: AccessToken is temp');
-    });    
-});
